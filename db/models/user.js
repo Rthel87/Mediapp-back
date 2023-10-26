0882fe92abf40850f61c852a10819407d868a4c7
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-10-26 17:41:33 -0300

    MER: Se cambia modelo 'User' según nuevo MER vB

--- db/models/user.js
+++ db/models/user.js
@@ -17,7 +17,8 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'User',
