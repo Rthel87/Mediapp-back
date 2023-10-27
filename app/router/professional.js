b96f315be8f599cae2c6dc19fbc4c037437a7551
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-10-27 01:39:39 -0300

    CRUD: Se agregan servicios 'CRUD' para los profesionales

---
+++ app/router/professional.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const professionalController = require('../../controllers/professionalController');
+
+router.get('/', (req, res) => {
+  professionalController.index(req, res);
+});
+
+router.post('/', (req, res) => {
+  professionalController.create(req, res);
+});
+
+router.get('/:id', (req, res) => {
+  professionalController.show(req, res);
+})
+
+router.put('/:id', (req, res) => {
+  professionalController.update(req, res);
+});
+
+router.patch('/:id', (req, res) => {
+  professionalController.update(req, res);
+})
+
+router.delete('/:id', (req, res) => {
+  professionalController.destroy(req, res);
+})
+
+module.exports = router;
