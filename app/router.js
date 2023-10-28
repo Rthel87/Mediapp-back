53ddee727278c9a964979921ceff7fcdc840f780
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-10-28 04:53:28 -0300

    Router: Se cambian nombres de rutas al plural

--- app/router.js
+++ app/router.js
@@ -3,9 +3,11 @@ const auth = require('./router/auth');
 const verifyToken = require('./router/verifyToken');
 const user = require('./router/user');
 const question = require('./router/question');
+const professional = require('./router/professional');
 
 router.use('/login', auth);
-router.use('/user', verifyToken, user);
-router.use('/question', verifyToken, question);
+router.use('/users', verifyToken, user);
+router.use('/questions', verifyToken, question);
+router.use('/professionals', verifyToken, professional);
 
 module.exports = router;
