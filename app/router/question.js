259684d65bfebc6f41379c5c7e0c11fa3f67a2ea
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-10-29 05:49:06 -0300

    Router: Se corrigen direcciones de 'Question'

--- app/router/question.js
+++ app/router/question.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const questionController = require('../controllers/questionController');
 
-router.get('/', (res, req) => {
+router.get('/', (req, res) => {
   questionController.index(req, res);
 });
 
